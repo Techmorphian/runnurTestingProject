8d22a2913c27d71bc65da932797ccad35ed1448e
Techmorphian <sonali@emgeesonss-imac.local>
2016-07-28 10:44:44 +0530

    aa

--- runnurTestingProject/ViewController.swift
+++ runnurTestingProject/ViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    func test()
+    {
+        
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
